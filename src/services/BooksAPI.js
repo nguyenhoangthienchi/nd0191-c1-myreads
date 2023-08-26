20fa7605cc74426db585539ce44be7dc59d5b563
nguyenhoangthienchi <jayesd3v@gmail.com>
2023-08-26 22:40:48 +0700

    restructure project

--- src/services/BooksAPI.js
+++ src/services/BooksAPI.js
@@ -1,11 +1,11 @@
-const api = "https://reactnd-books-api.udacity.com";
+const api = 'https://reactnd-books-api.udacity.com';
 
 let token = localStorage.token;
 
 if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
 
 const headers = {
-  Accept: "application/json",
+  Accept: 'application/json',
   Authorization: token,
 };
 
@@ -21,20 +21,20 @@ export const getAll = () =>
 
 export const update = (book, shelf) =>
   fetch(`${api}/books/${book.id}`, {
-    method: "PUT",
+    method: 'PUT',
     headers: {
       ...headers,
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ shelf }),
   }).then((res) => res.json());
 
 export const search = (query, maxResults) =>
   fetch(`${api}/search`, {
-    method: "POST",
+    method: 'POST',
     headers: {
       ...headers,
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ query, maxResults }),
   })
