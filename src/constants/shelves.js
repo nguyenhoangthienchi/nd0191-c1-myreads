b305325d536d690c5223aaf3361eb5ecce5fc07e
nguyenhoangthienchi <jayesd3v@gmail.com>
2023-08-26 22:41:13 +0700

    add constants for project

---
+++ src/constants/shelves.js
@@ -0,0 +1,7 @@
+const shelves = [
+  { id: 0, title: 'Currently Reading', shelfName: 'currentlyReading' },
+  { id: 1, title: 'Want to Read', shelfName: 'wantToRead' },
+  { id: 2, title: 'Read', shelfName: 'read' },
+];
+
+export { shelves };
